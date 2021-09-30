bfa3c8bde03eb43df27beeda80afc593d311bcad
PushStone <76048647+pogihae@users.noreply.github.com>
2021-10-01 01:06:49 +0900

    Make WRG status

--- src/MyProtocol.java
+++ src/MyProtocol.java
@@ -1,48 +1,54 @@
 import java.util.*;
 
 /** MyProtocol
- * Enum for each operators and status
+ *
+ * Enum for each operator and status
  * Making message using the rule
  * */
 public class MyProtocol {
 
     /** Operator in protocol*/
-    public static enum Operator {
+    public enum Operator {
         ADD('+'), MIN('-'), DIV('/'), MUL('*');
 
         Operator(final char opr) {
             this.opr = opr;
         }
 
-        private char opr;
+        private final char opr;
 
         public static String convertToType(char opr) {
             for(Operator op : Operator.values()) {
                 if(op.opr == opr) return op.name();
             }
-            return null;
+            return "?";
+        }
+
+        public char getOpr() {
+            return opr;
         }
     }
     /** Status in protocol*/
-    public static enum Status {
-        ANS(0), DIV0(1), ARG(2);
+    public enum Status {
+        ANS(0), DIV0(1), ARG(2), WRG(3);
 
         Status(final int code) {
             this.code = code;
         }
 
-        private int code;
+        private final int code;
 
         public static String convertToType(int code) {
             for(Status s : Status.values()) {
                 if(code == s.code) return s.name();
             }
-            return null;
+            return "?";
         }
     }
 
     /** makeRequest
      * make request message using rule in protocol
+     *
      * @return formed request message
      * */
     public static String makeRequest(Queue<Character> operators, Queue<Double> operands) {
@@ -58,17 +64,18 @@ public class MyProtocol {
         }
 
         msg.setLength(msg.length() - 1);
-        return msg.toString() + "\n";
+        return msg + "\n";
     }
 
     /** makeResponse
      * make response message using rule in protocol
+     *
      * @return formed response message
      * */
     public static String makeResponse(int code, String content) {
         StringBuffer msg = new StringBuffer();
-        msg.append(Status.convertToType(code)+" ");
+        msg.append(Status.convertToType(code)+": ");
         msg.append(content);
-        return msg.toString() + "\n";
+        return msg + "\n";
     }
 }
